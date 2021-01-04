a3a0bf13c0fd58a65d05a6ce036ace6c95736f17
LAPTOP-V066EPMU\HP <1425436550@qq.com>
2021-01-04 16:29:50 +0800

    no message

---
+++ ActivityServiceImpl.java
@@ -0,0 +1,21 @@
+package cn.smbms.pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+//add func1
+@Service("activityServiceImpl")
+public class ActivityServiceImpl {
+	//add func2
+	public List<String> plpList(){
+		List<String> list = new ArrayList<String>();
+		return list;
+	}
+
+	//add func3
+	public List<String> sadeList(){
+		List<String> list = new ArrayList<String>();
+		return list;
+	}
+}
