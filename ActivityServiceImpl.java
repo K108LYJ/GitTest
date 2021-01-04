21a966f6f74f557a93bceec77b683470c8615200
LAPTOP-V066EPMU\HP <1425436550@qq.com>
2021-01-04 16:31:23 +0800

    no message

--- ActivityServiceImpl.java
+++ ActivityServiceImpl.java
@@ -4,18 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-//add func1
-@Service("activityServiceImpl")
-public class ActivityServiceImpl {
-	//add func2
-	public List<String> plpList(){
-		List<String> list = new ArrayList<String>();
-		return list;
-	}
 
-	//add func3
-	public List<String> sadeList(){
-		List<String> list = new ArrayList<String>();
-		return list;
-	}
+public class ActivityServiceImpl {
+
 }
